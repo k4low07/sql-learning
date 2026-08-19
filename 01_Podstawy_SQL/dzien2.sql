817e590625e0781eb99d53b20d12766b60b1aaf6
Kacper <k.wolak1993@gmail.com>
2026-08-19 06:43:02 +0200

    update

--- 01_Podstawy_SQL/dzien2.sql
+++ 01_Podstawy_SQL/dzien2.sql
@@ -71,8 +71,160 @@ UPDATE tasks.city_country AS cc JOIN sakila.country AS c
 SET cc.country = c.country
 WHERE cc.country IS NULL;
 
-SELECT cc.country as nowa_tabela, c.country as stara_tabela
-from tasks.city_country cc join sakila.country c
-using (country_id)
-where country_id =82;
+SELECT cc.country AS nowa_tabela, c.country AS stara_tabela
+FROM tasks.city_country cc
+         JOIN sakila.country c
+              USING (country_id)
+WHERE country_id = 82;
 
+-- CREATE TABLE FOR TRYING DELETE
+
+DROP TABLE IF EXISTS tasks.films_to_be_cleaned;
+
+CREATE TABLE tasks.films_to_be_cleaned
+    LIKE sakila.film;
+
+INSERT INTO tasks.films_to_be_cleaned
+SELECT *
+FROM sakila.film;
+
+-- 1000 rows
+SELECT COUNT(*)
+FROM tasks.films_to_be_cleaned;
+
+DELETE fc
+FROM tasks.films_to_be_cleaned AS fc
+         JOIN sakila.film_category AS fcat USING (film_id)
+WHERE fcat.category_id IN (1, 5, 7, 9)
+  AND fc.length < 60
+  AND fc.rating NOT IN ('NC-17', 'PG');
+
+-- tables without 16 rows after 'delete'
+SELECT COUNT(*)
+FROM tasks.films_to_be_cleaned;
+
+SELECT *
+FROM tasks.films_to_be_cleaned;
+
+
+-- checking after delete
+SELECT *
+FROM tasks.films_to_be_cleaned AS fc
+         JOIN sakila.film_category AS fcat USING (film_id)
+WHERE fcat.category_id IN (1, 5, 7, 9)
+  AND fc.length < 60
+  AND fc.rating NOT IN ('NC-17', 'PG');
+
+INSERT INTO tasks.films_to_be_cleaned
+SELECT f.*
+FROM sakila.film AS f
+         JOIN sakila.film_category AS fc
+              USING (film_id)
+WHERE category_id IN (1, 5, 7, 9)
+  AND length < 60
+  AND rating NOT IN ('NC-17', 'PG');
+
+
+-- create table
+
+DROP TABLE IF EXISTS tasks.california_payments;
+
+CREATE TABLE tasks.california_payments
+    LIKE sakila.payment;
+
+INSERT INTO tasks.california_payments
+SELECT p.*
+FROM sakila.payment AS p
+         JOIN sakila.customer AS c
+              ON p.customer_id = c.customer_id
+         JOIN sakila.address AS a
+              ON c.address_id = a.address_id
+WHERE a.district = 'California';
+
+SELECT *
+FROM tasks.california_payments;
+
+TRUNCATE TABLE tasks.california_payments;
+
+
+SELECT address.district
+FROM address
+WHERE district = 'California';
+
+INSERT INTO tasks.california_payments
+SELECT p.*
+FROM sakila.payment AS p
+         JOIN sakila.customer AS c USING (customer_id)
+         JOIN sakila.address AS a USING (address_id)
+WHERE a.district = 'California';
+
+
+SELECT cp.*
+FROM tasks.california_payments cp
+         JOIN sakila.customer AS c USING (customer_id)
+         JOIN sakila.address AS a USING (address_id)
+WHERE a.district = 'California';
+
+
+-- CREATE TABLE FOR DELETE CASCADE
+CREATE TABLE tasks.buildings
+(
+    building_no   INT PRIMARY KEY,
+    building_name VARCHAR(255),
+    address       VARCHAR(255)
+);
+
+CREATE TABLE tasks.rooms
+(
+    room_no     INT,
+    room_name   VARCHAR(255) NOT NULL,
+    building_no INT          NOT NULL,
+
+    -- Tutaj znajduje się definicja klucza obcego
+    FOREIGN KEY (building_no)
+
+        -- do której kolumny oraz tabeli się odnosimy
+        REFERENCES tasks.buildings (building_no)
+
+        /*
+        * poniższy fragment mówi, że gdy rekord zostanie
+        * usunięty z buildings,
+        * odnoszący się do niego wiersz z rooms - również
+        */
+        ON DELETE CASCADE
+);
+
+
+INSERT INTO tasks.buildings(building_no, building_name, address)
+VALUES (1, 'ACME Headquarters', '3950 North 1st Street CA 95134'),
+       (2, 'ACME Sales', '5000 North 1st Street CA 95134');
+
+INSERT INTO tasks.rooms(room_no, room_name, building_no)
+VALUES (1, 'Amazon', 1),
+       (2, 'War Room', 1),
+       (3, 'Office of CEO', 1),
+       (4, 'Marketing', 2),
+       (5, 'Showroom', 2);
+
+
+SELECT *
+FROM tasks.buildings;
+SELECT *
+FROM tasks.rooms;
+
+
+-- delete
+DELETE FROM tasks.buildings WHERE building_no = 1; -- usuwamy wiersze
+SELECT * FROM tasks.buildings;
+SELECT * FROM tasks.rooms;
+
+
+USE information_schema;
+
+SELECT
+  UNIQUE_CONSTRAINT_SCHEMA,
+  TABLE_NAME,
+  REFERENCED_TABLE_NAME
+FROM
+  referential_constraints
+WHERE delete_rule = 'CASCADE'
